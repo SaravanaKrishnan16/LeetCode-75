1350662fbe5595ff6e0c6f57956661e7df1a3940
Saravana Krishnan J <168951123+SaravanaKrishnan16@users.noreply.github.com>
2025-05-29 18:53:41 +0530

    Binary Tree  and  Depth-First Search

---
+++ Path Sum III.java
@@ -0,0 +1,39 @@
+Given the root of a binary tree and an integer targetSum, return the number of paths where the sum of the values along the path equals targetSum.
+
+The path does not need to start or end at the root or a leaf, but it must go downwards (i.e., traveling only from parent nodes to child nodes).
+
+Example 1:
+
+Input: root = [10,5,-3,3,2,null,11,3,-2,null,1], targetSum = 8
+Output: 3
+Explanation: The paths that sum to 8 are shown.
+Example 2:
+
+Input: root = [5,4,8,11,null,13,4,7,2,null,null,5,1], targetSum = 22
+Output: 3
+
+Constraints:
+
+The number of nodes in the tree is in the range [0, 1000].
+-109 <= Node.val <= 109
+-1000 <= targetSum <= 1000
+
+----------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+class Solution {
+    int total = 0;
+    public int pathSum(TreeNode root, int sum) {
+        if(root == null) return 0;
+        helper(root, sum, 0);
+        pathSum(root.left, sum);
+        pathSum(root.right, sum);
+        return total;
+    }
+    void helper(TreeNode root, int sum, long curr) {
+        if(root == null) return;
+        curr += root.val;
+        if(curr == sum) total++;
+        helper(root.left, sum, curr);
+        helper(root.right, sum, curr);
+    }
+}
